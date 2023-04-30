5f77aabf256fe5f0548a9d63ff86f19ff27e09dd
Oliver Wipfli <oliver.wipfli@leichteralsluft.ch>
2023-04-30 20:03:09 +0200

    Optimize

--- dist/maplibre-gl-dev.js
+++ dist/maplibre-gl-dev.js
@@ -26825,21 +26825,25 @@ class CanvasComparer {
         this.canvas2 = new OffscreenCanvas(100, 100);
         this.ctx1 = this.canvas1.getContext('2d', { willReadFrequently: true });
         this.ctx2 = this.canvas2.getContext('2d', { willReadFrequently: true });
-        this.ctx1.font = '24px Arial';
-        this.ctx2.font = '24px Arial';
+        this.ctx1.font = '12px Arial';
+        this.ctx2.font = '12px Arial';
     }
     compareCanvases(string1, string2) {
         this.ctx1.clearRect(0, 0, this.canvas1.width, this.canvas1.height);
         this.ctx2.clearRect(0, 0, this.canvas2.width, this.canvas2.height);
-        this.ctx1.fillText(`${string1}${string2}`, 10, 40);
+        this.ctx1.fillText(`${string1}${string2}`, 0, 20);
+        const offset1 = this.ctx1.measureText(`${string1}${string2}`).width;
         const parts = [string1, string2];
-        let offset = 0;
+        let offset2 = 0;
         for (const part of parts) {
-            this.ctx2.fillText(part, 10 + offset, 40);
-            offset += this.ctx2.measureText(part).width;
+            this.ctx2.fillText(part, offset2, 20);
+            offset2 += this.ctx2.measureText(part).width;
         }
-        const imageData1 = this.ctx1.getImageData(0, 0, this.canvas1.width, this.canvas1.height);
-        const imageData2 = this.ctx2.getImageData(0, 0, this.canvas2.width, this.canvas2.height);
+        if (offset1 !== offset2) {
+            return false;
+        }
+        const imageData1 = this.ctx1.getImageData(0, 0, offset2, 30);
+        const imageData2 = this.ctx2.getImageData(0, 0, offset2, 30);
         if (imageData1.data.length !== imageData2.data.length) {
             return false;
         }
